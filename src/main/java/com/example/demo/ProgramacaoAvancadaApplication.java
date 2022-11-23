f053b79b88604d49757baf75f8fdc22e2f61daf8
Eduardo <eduh.cavalcanti@hotmail.com>
2022-11-23 20:51:25 -0300

    Implementação do Spring Boot

---
+++ src/main/java/com/example/demo/ProgramacaoAvancadaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacaoAvancadaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProgramacaoAvancadaApplication.class, args);
+	}
+
+}
